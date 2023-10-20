82173e79ff401274021c8f538ae5c79be86d4781
severinbuchser <severinbuchser@gmail.com>
2023-10-21 00:25:39 +0200

    feat: add ssh project with ssh user command

---
+++ src/commands/ssh/Program.cs
@@ -0,0 +1,25 @@
+﻿using Server.GitShell.Commands.SSH.User;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Server.GitShell.Commands.SSH;
+
+public class Program
+{
+    static int Main(string[] args)
+    {
+        var app = new CommandApp();
+        app.Configure(config => 
+        {
+            config.AddCommand<SSHUser>("user");
+        });
+        try 
+        {
+            return app.Run(args);
+        }
+        catch (Exception e) {
+            AnsiConsole.WriteException(e);
+            return 128;
+        }
+    }
+}
---
+++ src/commands/ssh/user/SSHUser.cs
@@ -0,0 +1,11 @@
+using Spectre.Console.Cli;
+
+namespace Server.GitShell.Commands.SSH.User;
+
+class SSHUser : Command
+{
+    public override int Execute(CommandContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
