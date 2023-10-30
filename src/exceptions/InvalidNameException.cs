b9eb1fbbf32aa3576d242d97186ee935b62db4fb
severinbuchser <severinbuchser@gmail.com>
2023-10-30 23:59:04 +0100

    test: improve test coverage

--- src/exceptions/InvalidNameException.cs
+++ src/exceptions/InvalidNameException.cs
@@ -2,11 +2,5 @@ namespace SimpleGitShellrary.Exceptions;
 
 public class InvalidNameException : Exception
 {
-    public string Kind { get; }
-
-    public InvalidNameException(string kind, string name) : base($"The {kind} name \"{name}\" is not valid. The name can only contain word characters, digits and hyphens (\"-\").")
-    {
-        Kind = kind;
-    }
-
+    public InvalidNameException(string kind, string name) : base($"The {kind} name \"{name}\" is not valid. The name can only contain word characters, digits and hyphens (\"-\").") { }
 }
